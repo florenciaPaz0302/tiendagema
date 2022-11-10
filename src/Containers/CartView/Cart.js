43401a132bcccd87148acced6e789f7eba3698dc
Maria Paz <florcita_paz@hotmail.com>
2022-11-10 09:03:30 -0300

    cart

--- src/Containers/CartView/Cart.js
+++ src/Containers/CartView/Cart.js
@@ -1,7 +1,58 @@
-import React from "react"
+import React, { useContext } from "react";
+import { Context } from "../../Context/CustomContext";
+import { Link } from "react-router-dom";
+import "./Cart.css";
+import { db } from "../../firebase/firebase";
+import { collection, addDoc, serverTimestamp, doc, updateDoc } from "firebase/firestore"
 
-export const Cart = () => {
+
+export const Cart = ({ estilo, color }) => {
+    const { cart, total, clear } = useContext(Context);
+
+    const comprador = {
+        nombre: `maria`,
+        apellido: `mitre`,
+        email: `b@b.com`
+    };
+    const finalizarCompra = ()=>{
+        const ventasCollection = collection(db,"ventas");
+        addDoc(ventasCollection,{
+            comprador,
+            items:[{nombre:'remera'},{nombre:'short'}],
+            total: 200,
+            date:serverTimestamp()
+        })
+        .then(result=>{
+            console.log(result.id);
+          })
+        .catch(e => {
+            console.log('todo mal');
+            console.log(e);
+          });
+          
+        clear();
+        }
+        const actualizarStock = ()=>{
+            const updateStock = doc(db, "productos","tr8nxpCyuMbOsHfoyZuE")
+            updateDoc(updateStock,{stock:80})
+          }
     return (
-        <h1>cart</h1>
-    )
-}
+        <>
+          {cart.length === 0 ? (
+            <>
+              <h1 className={estilo ? "blue" : "red"}>
+                No agregaste productos <Link to="/">ingresa aqui</Link>
+              </h1>
+              <h2 className={`tamano ${color || ''}`}>Gracias por visitarnos</h2>
+            </>
+          ) : (
+            <>
+              {cart.map((producto) => (
+                <h1 key={producto.id}>{producto.title}</h1>
+              ))}
+            </>
+          )}
+          <button onClick={actualizarStock}>finalizar compra</button>
+        </>
+      );
+    };
